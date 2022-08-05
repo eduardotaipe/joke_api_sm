8ce7448c417867c504c23bc4e8362107fb170d74
eduardotaipe <eduardotl@eduardotaipe.com>
2022-08-05 14:46:09 -0500

    fix pytest deprecation warning invalid escape \d

--- app/api/deps.py
+++ app/api/deps.py
@@ -18,7 +18,7 @@ def get_db() -> Generator:
 def get_query_as_numbers_list(numbers: str = Query(None)) -> Optional[List[int]]:
     if numbers is None:
         return None
-    regex = re.compile("^\d+(,\d+){0,}$")
+    regex = re.compile(r"^\d+(,\d+){0,}$")
     regex_result = re.search(regex, numbers)
 
     if regex_result is None:
